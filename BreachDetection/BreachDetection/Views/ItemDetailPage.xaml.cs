73f616c78c059294dfd6168dfc28af187b4f3953
Chris Marassovich <chris.marassovich@distillerysoftware.com>
2024-10-21 16:11:25 +1100

    Add project files.

---
+++ BreachDetection/BreachDetection/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BreachDetection.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BreachDetection.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ BreachDetection/BreachDetection/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BreachDetection.Models;
+using BreachDetection.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BreachDetection.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
